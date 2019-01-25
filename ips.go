0e05cb81ccf4b38ec0eb7ce2629f71701fd33766
Jakub Jarosz <jakub.jarosz@postpro.net>
2019-01-25 21:08:50 +0000

    Adding structs to hold fetched data

--- ips.go
+++ ips.go
@@ -1,4 +1,20 @@
 package main
 
+type ipv4 struct {
+	IPv4prefix string `json:"ipv4_prefix"`
+	Region     string `json:"region"`
+	Service    string `json:"service"`
+}
 
+type ipv6 struct {
+	IPv6prefix string `json:"ipv6_prefix"`
+	Region     string `json:"region"`
+	Service    string `json:"service"`
+}
 
+type ipranges struct {
+	SyncToken    string `json:"syncToken"`
+	CreateDate   string `json:"createDate"`
+	IPv4prefixes []ipv4 `json:"prefixes"`
+	IPv6prefixes []ipv6 `json:"ipv6_prefixes"`
+}
